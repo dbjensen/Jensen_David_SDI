df650ac7fa1ded8ea2d05b79115b0a9783b02de6
burntbbq <dbjensen@fullsail.com>
2015-01-08 16:15:55 -0700

    Added variable and user prompts
    Added variable and user prompts

--- GoToTraining01/js/script.js
+++ GoToTraining01/js/script.js
@@ -6,3 +6,12 @@ Go To Training Week 01
  */
 
 alert("Testing to make sure it's connected.");
+
+console.log("This is the console.");
+
+var userInput = prompt("Please type in your name:");
+
+console.log("Welcome "+userInput);
+
+alert("Welcome "+userInput);
+
