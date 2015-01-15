244992a71b8036ab43312af4d884c06af4337e3e
burntbbq <dbjensen@fullsail.com>
2015-01-15 14:05:40 -0700

    Video Concatenating Strings
    Video Concatenating Strings

---
+++ VidConcatenating/js/script.js
@@ -0,0 +1,19 @@
+// Video Concatenating
+
+var firstName = "David";
+var lastName = "Jensen";
+var fullName = firstName + " " + lastName;
+
+console.log(fullName)
+
+var a = "6";
+var b = "7";
+
+var result = a + b;
+console.log(result);
+
+var c = 6;
+var d = 7;
+
+var result1 = c + d;
+console.log(result1);
