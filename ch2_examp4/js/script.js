c730a984d14d658bbb1d86126c614688c29707d0
burntbbq <dbjensen@fullsail.com>
2015-01-15 09:59:50 -0700

    Chapter 2 example 4.
    Fahrenheit to Centigrade Converter.

---
+++ ch2_examp4/js/script.js
@@ -0,0 +1,11 @@
+// Chapter 2, Example 4
+// TRY IT OUT: Fahrenheit to Centigrade
+
+// Equation is C = 5/9 (F - 32).
+
+var degFahren = prompt("Enter the degrees in Fahrenheit",50);
+var degCent;
+
+degCent = 5/9 * (degFahren - 32);
+
+alert(degCent + " Degrees Centigrade");
