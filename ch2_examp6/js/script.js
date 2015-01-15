8c8188c8249304c26a332f16ea9c10beb73690d4
burntbbq <dbjensen@fullsail.com>
2015-01-15 11:15:51 -0700

    Chapter 2, example 6
    TRY IT OUT: Converting Strings to Numbers.

---
+++ ch2_examp6/js/script.js
@@ -0,0 +1,9 @@
+// Chapter 2, example 6
+//TRY IT OUT: Making the Temperature Converter User-Friendly
+
+var degFahren = prompt("Enter the degrees in Fahrenheit", 50);
+var degCent;
+
+degCent = 5/9 * (degFahren - 32);
+
+alert(degFahren + "\xB0 Fahrenheit is " + degCent + "\xB0 Centigrade");
