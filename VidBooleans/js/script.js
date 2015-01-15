06a6269121bc7c8038310da8bc93bada117727a2
burntbbq <dbjensen@fullsail.com>
2015-01-14 18:16:59 -0700

    Adding Boolean Video Exercise
    Adding Boolean Video Exercise

---
+++ VidBooleans/js/script.js
@@ -0,0 +1,11 @@
+// Variables of Different Types, Numbers, Booleans, and Strings
+
+var age = 39;    //Number variable
+
+var isStudent = false;      // Boolean Variable
+
+var firstName = "kermit";
+
+var phrase = 'I don\'t know,\n You\'ll have to speak with the professor';
+
+console.log(phrase);
