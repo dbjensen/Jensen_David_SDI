ed0edc88b3710c4e2dcd56589300378e0a045178
burntbbq <dbjensen@fullsail.com>
2015-01-15 14:34:38 -0700

    Video Assignment Operators
    Video Assignment Operators

---
+++ VidAssignmentOperators/js/script.js
@@ -0,0 +1,14 @@
+// Assignment Operators
+
+var a = 3;
+var b = 2;
+var c = 5;
+
+a += 4;             // a = a + 4
+console.log(a);
+
+b ++;               // b = b + 1
+console.log(b);
+
+c --;               // c = c - 1
+console.log(c);
