c6baed24185a5f289248c8a1786b620d08e7fcdc
burntbbq <dbjensen@fullsail.com>
2015-01-14 17:37:54 -0700

    Submitted Defining Variables and Fixed Names
    Submitted Defining Variables and Fixed Names of 2 Video Exercises.

---
+++ VidDefiningVariables/js/script.js
@@ -0,0 +1,11 @@
+// Defining Variables
+
+var age;    //declaration
+age = 39;   //definition
+
+//storage container = value
+
+var yearOfBirth = 1970;     //declaring and defining on the same line
+
+console.log(age);
+console.log(yearOfBirth);
