a729870c42380174075a7489be4ec113b5dd221f
burntbbq <dbjensen@fullsail.com>
2015-01-15 10:36:55 -0700

    Chapter 2, example 5.
    TRY IT OUT: Concatenating Strings.

---
+++ ch2_examp5/js/script.js
@@ -0,0 +1,10 @@
+// Chapter 2, example 5
+// TRY IT OUT: Concatenating Strings
+
+var greetingString = "Hello";
+var myName = prompt("Please enter your name", "Bob");
+var concatString;
+
+document.write(greetingString + " " + myName + "<br>");
+concatString = greetingString + " " + myName;
+document.write(concatString);
