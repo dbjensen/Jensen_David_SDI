51f4484705696f0b4db1b3de778ae7a910499f93
burntbbq <dbjensen@fullsail.com>
2015-01-15 12:28:17 -0700

    Chapter 2, example 8
    TRY IT OUT: An Array.

---
+++ ch2_examp8/js/script.js
@@ -0,0 +1,14 @@
+// Chapter 2, example 8
+// TRY IT OUT: An Array
+
+var myArray = new Array();
+myArray[0] = "Bob";
+myArray[1] = "Pete";
+myArray[2] = "Paul";
+
+document.write("myArray[0] = " + myArray[0] + "<BR>");
+document.write("myArray[2] = " + myArray[2] + "<BR>");
+document.write("myArray[1] = " + myArray[1] + "<BR>");
+
+myArray[1] = "Mike";
+document.write("myArray[1] changed to " + myArray[1]);
