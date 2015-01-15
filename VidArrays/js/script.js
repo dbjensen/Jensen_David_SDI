a9722408cb9ebc52f04e5d417b239b95211b6f26
burntbbq <dbjensen@fullsail.com>
2015-01-14 18:56:22 -0700

    Adding Array Exercise
    Adding Array Exercise

---
+++ VidArrays/js/script.js
@@ -0,0 +1,15 @@
+// Arrays
+
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"];       // Indexes 0,1,2,3
+
+alert(muppetNames);
+
+// Array Access Notation to Access the Collection
+
+alert(muppetNames[1]);
+
+muppetNames[1] = "Rizzo";
+muppetNames[2] = "Fozzy";
+muppetNames[3] = "Miss Piggy";
+
+console.log(muppetNames);
