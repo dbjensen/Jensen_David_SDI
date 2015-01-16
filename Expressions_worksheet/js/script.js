5285330a80d3d4a4018c82a04faccab5a9d2c40f
burntbbq <dbjensen@fullsail.com>
2015-01-16 09:30:48 -0700

    Minor changes to print
    Minor changes to print

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,7 +1,9 @@
-// Dog Years
+// David Jensen
+// 20150115
 
 var humanYears = prompt("Please enter Sparky\'s age in human years:", "4");     // Declaring and Defining Variable
 var dogYears = 7;                                                               // Declaring and Defining Variable
                         // Declaring and Defining Variable Including Math Expression
-var result = "Sparky is " + humanYears + " human years old \n which is " + humanYears * dogYears + " in dog years";                                                 // Defining Operation
+var result = "Sparky is " + humanYears + " human years old \nwhich is " + humanYears * dogYears + " in dog years";                                                 // Defining Operation
 console.log(result);                                                            // Printing to Console
+alert(result);                                                                  // Printing to Alert for User
