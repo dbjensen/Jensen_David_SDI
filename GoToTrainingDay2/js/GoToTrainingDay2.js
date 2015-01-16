1204e897152e40a5152fe587e38822980a4510f0
burntbbq <dbjensen@fullsail.com>
2015-01-16 10:04:13 -0700

    Prompted user for name
    Used and alert to say hello.

--- GoToTrainingDay2/js/GoToTrainingDay2.js
+++ GoToTrainingDay2/js/GoToTrainingDay2.js
@@ -5,3 +5,14 @@ Go To Training Day #2
 2015/01/16
  */
 // alert("Testing to see if the JS file is attached to the HTML.");
+
+// Create an age calculator
+
+// Ask the user their name
+var name = prompt("Please type in your name:");
+
+// console.log out the name
+console.log(name);
+
+// Welcome the user with an alert
+alert("Welcom "+name+"!  Let's figure out how old you are.")
