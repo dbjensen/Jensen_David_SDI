a683c316bcbd4b450714f6ca8f3d3d9c0c25ca23
burntbbq <dbjensen@fullsail.com>
2015-01-20 10:42:43 -0700

    Video: Relational Expressions.
    Created code based off video.  Comparing relationship of variables.

--- WEEK3/js/SunnyBeach.js
+++ WEEK3/js/SunnyBeach.js
@@ -2,16 +2,18 @@
  /*
 David Jensen
 SDI Section #3
-Video: Conditionals Example
+Video: Relational Expressions
 2015/01/20
  */
 //alert("Testing to see if the JS file is attached to the HTML.");
 
- var oldEnough = false;
+ var kidHeight = 40;
+ var minHeight = 48;
 
  //if the child is old enough, print tot he console "you can ride!"
- if(oldEnough){
+ //if the kid is over 48 inches in height.
+ if(kidHeight > minHeight){
   //code performed if condition is true.
   console.log("you can ride the coaster!");
  }
- console.log("what comes after");
+ console.log("You may not ride.");
