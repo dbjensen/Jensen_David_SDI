d149b3d19142ea8c3ae5658e4ee3e2bcd071cb43
burntbbq <dbjensen@fullsail.com>
2015-01-20 10:49:52 -0700

    Video: Condition with Expressions.
    Created code based off video.  Using variables in an expression for the
    condition.

--- WEEK3/js/SunnyBeach.js
+++ WEEK3/js/SunnyBeach.js
@@ -2,18 +2,19 @@
  /*
 David Jensen
 SDI Section #3
-Video: Relational Expressions
+Video: Condition with Expressions
 2015/01/20
  */
 //alert("Testing to see if the JS file is attached to the HTML.");
 
- var kidHeight = 40;
+ var kidHeight = 47;
  var minHeight = 48;
+ var sneakerLift = 2;
 
  //if the child is old enough, print tot he console "you can ride!"
  //if the kid is over 48 inches in height.
- if(kidHeight > minHeight){
+ if(kidHeight+sneakerLift > minHeight){
   //code performed if condition is true.
   console.log("you can ride the coaster!");
  }
- console.log("You may not ride.");
+ 
