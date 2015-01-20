87eb733e0acb9833f4216b3b97ba18bbe3b55eb6
burntbbq <dbjensen@fullsail.com>
2015-01-20 09:47:46 -0700

    Video: Conditional Logic
    Created code based of video.

---
+++ WEEK3/js/SunnyBeach.js
@@ -0,0 +1,18 @@
+
+ /*
+David Jensen
+SDI Section #3
+Video: Conditional Logic Concepts
+2015/01/20
+ */
+//alert("Testing to see if the JS file is attached to the HTML.");
+
+ var choice1 = "goToTheBeach";
+ var choice2 = "goToTheMovies";
+ var sunny = true;
+
+ if(sunny){
+  console.log(choice1);
+ }else{
+  console.lo(choice2);
+ }
