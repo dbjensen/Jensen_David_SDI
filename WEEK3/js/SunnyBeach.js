77c9976fb33a261d0c1c53b9fd9f4e62d92be62e
burntbbq <dbjensen@fullsail.com>
2015-01-20 10:57:49 -0700

    Video: If and else
    Created code based on video.  Adding else statement to the if statement.

--- WEEK3/js/SunnyBeach.js
+++ WEEK3/js/SunnyBeach.js
@@ -2,19 +2,20 @@
  /*
 David Jensen
 SDI Section #3
-Video: Condition with Expressions
+Video: If and else
 2015/01/20
  */
 //alert("Testing to see if the JS file is attached to the HTML.");
 
- var kidHeight = 47;
+ var kidHeight = 50;
  var minHeight = 48;
- var sneakerLift = 2;
+
 
  //if the child is old enough, print tot he console "you can ride!"
  //if the kid is over 48 inches in height.
- if(kidHeight+sneakerLift > minHeight){
+ if(kidHeight > minHeight){
   //code performed if condition is true.
   console.log("you can ride the coaster!");
+ }else{
+  console.log("Sorry kid, you've got some growing to do first!")
  }
- 
