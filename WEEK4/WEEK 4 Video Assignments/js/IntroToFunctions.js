1443a8e7271638017ef1a61fbaf452e525d05b9d
burntbbq <dbjensen@fullsail.com>
2015-01-24 18:28:36 -0700

    Basic Function Structure
    Basic Function Structure

---
+++ WEEK4/WEEK 4 Video Assignments/js/IntroToFunctions.js
@@ -0,0 +1,28 @@
+
+ /*
+David Jensen
+SDI Section #3
+Basic Function Structure
+2015/01/24
+ */
+//alert("Testing to see if the JS file is attached to the HTML.");
+
+ function outPutMsg(){
+  console.log("Hello World");
+ }
+
+
+function calcArea(){
+ var width = 20;
+ var height = 30;
+ var area = width * height;
+ console.log(area);
+}
+
+
+
+
+
+
+
+
