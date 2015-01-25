1bb05c1dd9acc46bb2e0cd2887fabb25eccf62f8
burntbbq <dbjensen@fullsail.com>
2015-01-25 14:01:38 -0700

    Functions - Function vs Procedure
    Functions - Function vs Procedure

--- WEEK4/WEEK 4 Video Assignments/js/IntroToFunctions.js
+++ WEEK4/WEEK 4 Video Assignments/js/IntroToFunctions.js
@@ -7,15 +7,27 @@ Functions - Function vs Procedure
  */
 //alert("Testing to see if the JS file is attached to the HTML.");
 
-//This is a function
- function calcAreaF(width, height){
+ /*
+ var functionName = function(){
+    "code the function runs;
+ }
+ functionName();
+  */
+ //var functionName = function (){
+   //code to run
+ //}
+ //functionName();
+
+ var calcArea = function(width, height){    //defining function THIS HAS TO BE FIRST
+  //code for function to run
   var area = width * height;
   return area;
  }
 
-//This is a procedure
- function calcAreaP(width, height){
-  var area = width * height;
-  console.log(area);     //Nothing returned
- }
+ var a  = calcArea(20, 30);                 //invoking function THIS MUST COME AFTER DEFINING
+
+ console.log(a);
+
+
+
 
