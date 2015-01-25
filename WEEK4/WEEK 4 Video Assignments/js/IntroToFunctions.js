4b78c0dce1d4e5203a9a38331c58f80b6241d2a4
burntbbq <dbjensen@fullsail.com>
2015-01-25 13:45:54 -0700

    Functions - Function vs Procedure
    Functions - Function vs Procedure

--- WEEK4/WEEK 4 Video Assignments/js/IntroToFunctions.js
+++ WEEK4/WEEK 4 Video Assignments/js/IntroToFunctions.js
@@ -2,30 +2,20 @@
  /*
 David Jensen
 SDI Section #3
-Functions - Returning Values
+Functions - Function vs Procedure
 2015/01/25
  */
 //alert("Testing to see if the JS file is attached to the HTML.");
 
- var total = calcArea(30, 20);
-
- function calcArea(w, h){
-  var area = w * h;
-  return area;        //function spitting the info out
- }
- console.log(total);
-
-
-
- var power = calcPower(70, 190);
-
- function calcPower(s, m){
-  var p = s * m;
-  return p;
+//This is a function
+ function calcAreaF(width, height){
+  var area = width * height;
+  return area;
  }
 
- console.log(power);
-
-
-
+//This is a procedure
+ function calcAreaP(width, height){
+  var area = width * height;
+  console.log(area);     //Nothing returned
+ }
 
