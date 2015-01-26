5fb48314a9c2b6e2fc8e1d90843ad62568c147ff
burntbbq <dbjensen@fullsail.com>
2015-01-26 12:00:45 -0700

    WORKING WITH LOOPS
    WORKING WITH LOOPS

--- Loops/js/WhileLoops.js
+++ Loops/js/WhileLoops.js
@@ -41,6 +41,59 @@ console.log("--------------------For Loops--------------------");
 
 
 
+//WORKING WITH LOOPS
+ console.log("--------------------WHILE LOOPS--------------------");
+ var a = 1;
+
+ while (a < 1);{
+  console.log(a);
+  a++
+ }
+
+ //WORKING WITH LOOPS
+ console.log("--------------------DO WHILE LOOPS--------------------");
+
+ var a = 100;
+
+ do{
+  console.log(a);
+  a++
+ }while (a < 10);
+
+ var i = 1;        //setup of index
+
+ while (i < 10){   //check the condition
+  //
+  //
+  //
+  i++;             //incrementing the index
+ }
+
+
+ //WORKING WITH LOOPS
+ console.log("--------------------FOR LOOPS--------------------");
+
+for ( var i = 1 ; i < 10 ; i++ ){        //( setup index ; check condition ; increment index )
+ //
+ //
+ //
+}
+
+// the "break".  Jumps us out of the loop
+
+ for (var i = 1;i < 5000;i++){
+  //
+  //
+  if (i == 101){
+   break;
+  }
+  //
+ }
+
+// the "continue".  Done with current iteration, starts back up at top.
+
+
+
 
 
 
