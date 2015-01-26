3d0c767d3cb22f251a81accb9ef331fe60f1922f
burntbbq <dbjensen@fullsail.com>
2015-01-26 11:29:51 -0700

    for Loops
    for Loops

--- Loops/js/WhileLoops.js
+++ Loops/js/WhileLoops.js
@@ -2,15 +2,48 @@
  /*
 David Jensen
 SDI Section #3
-While Loops
+for Loops
 2015/01/26
  */
 //alert("Testing to see if the JS file is attached to the HTML.");
 
+ //while loops
+ console.log("--------------------Loops--------------------");
+ //while loops
 var b = 10;                                 //sets up the index
 
  while (b > 0){                             //checks the condition
   console.log(b +" kegs on the wall");
   b --;                                     //increments or decrements the index
  }
+
+ console.log("--------------------Do While Loops--------------------");
+ //do while loops
+var c = 10;
+
+ do{
+  console.log(c +" kegs on the wall");      //code runs before the condition
+  c--;                                      //
+ }
+ while (c > 0);                             //runs after code
+
+/*
+NOTICED!!!   while: while this condition is true run those instructions.
+NOTICED!!!   do while: do those instruction while this condition is true.
+ */
+
+//for loops
+console.log("--------------------For Loops--------------------");
+
+ for (var i= 10; i > 0; i --){
+  console.log(i +" kegs on the wall.");
+ }
+
+
+
+
+
+
+
+
 
