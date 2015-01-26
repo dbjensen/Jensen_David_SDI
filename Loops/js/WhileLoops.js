3ea027009ca6e3b679585dd7bc302e4060f2e628
burntbbq <dbjensen@fullsail.com>
2015-01-26 10:56:42 -0700

    While Loops
    While Loops

---
+++ Loops/js/WhileLoops.js
@@ -0,0 +1,16 @@
+
+ /*
+David Jensen
+SDI Section #3
+While Loops
+2015/01/26
+ */
+//alert("Testing to see if the JS file is attached to the HTML.");
+
+var b = 10;                                 //sets up the index
+
+ while (b > 0){                             //checks the condition
+  console.log(b +" kegs on the wall");
+  b --;                                     //increments or decrements the index
+ }
+
--- WEEK4/WEEK 4 Video Assignments/js/IntroToFunctions.js
+++ WEEK4/WEEK 4 Video Assignments/js/IntroToFunctions.js
@@ -2,32 +2,9 @@
  /*
 David Jensen
 SDI Section #3
-Functions - Function vs Procedure
+
 2015/01/25
  */
 //alert("Testing to see if the JS file is attached to the HTML.");
-
- /*
- var functionName = function(){
-    "code the function runs;
- }
- functionName();
-  */
- //var functionName = function (){
-   //code to run
- //}
- //functionName();
-
- var calcArea = function(width, height){    //defining function THIS HAS TO BE FIRST
-  //code for function to run
-  var area = width * height;
-  return area;
- }
-
- var a  = calcArea(20, 30);                 //invoking function THIS MUST COME AFTER DEFINING
-
- console.log(a);
-
-
 
 
