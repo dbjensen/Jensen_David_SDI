d88f140b1867372d3982bd4138047a3d0bde4cb8
burntbbq <dbjensen@fullsail.com>
2015-01-29 08:19:27 -0700

    GoTo Training Starting Files
    GoTo Training Starting Files with some further discussions

--- GoTo Training Day 4/WEEK 4 Video Assignments/js/day4.js
+++ GoTo Training Day 4/WEEK 4 Video Assignments/js/day4.js
@@ -6,3 +6,21 @@ Go To Training Day #4
 2015/01/28
  */
 //alert("Testing to see if the JS file is attached to the HTML.");
+
+//create a random # between two #s
+//user input min #
+var min = prompt("Let's find a random number between two numbers.\nPlease enter a min number:");
+
+
+ while(min==="" || isNaN(min)) {
+  //reprompt the user
+  min = prompt("Please do not leave blank and only use numbers");
+ }
+
+  do{
+   var name = prompt("Please type your name")
+  }while(max==="" || isNaN(max));
+
+
+
+ //!isnan();
